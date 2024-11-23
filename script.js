fcb65e7c0638674f3dfc18257455789624d0743c
Sotiris Tilos <stilos@gmail.com>
2024-11-23 17:01:48 +0200

    reach the exercise to a working stage no checking for number of rounds code runs infinitely

--- script.js
+++ script.js
@@ -1,88 +1,98 @@
-function getComputerChoice() {
-  let choice = ""
-  let rand = Math.floor(Math.random() * 3)
-
-  switch (rand) {
-    case 0:
-      choice = "Rock"
-      break
-    case 1:
-      choice = "Paper"
-      break
-    case 2:
-      choice = "Scissors"
-  }
-
-  return choice
-}
-
-function getHumanChoice() {
-  let choice = ""
-  let ans = prompt("What is your choice? 1-Rock, 2-Paper, 3-Scissors")
-
-  switch (parseInt(ans)) {
-    case 1:
-      choice = "Rock"
-      break
-    case 2:
-      choice = "Paper"
-      break
-    case 3:
-      choice = "Scissors"
-  }
-
-  return choice
-}
-
-
-
-function playGame() {
   let humanScore = 0
   let computerScore = 0
 
-  function playRound(humanChoice, computerChoice) {
+  container.style.cssText = "color: blue; background: white;";
+  let action = document.querySelector(".action");
+  let results = document.querySelector(".results");
+  let score = document.querySelector(".score");
+
+  let rock = document.querySelector("#rock");
+  rock.addEventListener("click", () => {
+    
+    playRound("Rock");
+    action.textContent =  "Rock clicked.";
+    score.textContent = `The score is: You ${humanScore} to Comp ${computerScore}`;
+  });
+
+  let paper = document.querySelector("#paper");
+  paper.addEventListener("click", () => {
+
+    playRound("Paper");
+    action.textContent = "Paper clicked.";
+    score.textContent = `The score is: You ${humanScore} to Comp ${computerScore}`;
+  });
+
+  let scissors = document.querySelector("#scissors");
+  scissors.addEventListener("click", () => {
+
+    playRound("Scissors")
+    action.textContent = "Scissors clicked.";
+    score.textContent = `The score is: You ${humanScore} to Comp ${computerScore}`;
+  });
+
+  function playRound(humanChoice) {
+    let computerSelection = getComputerChoice();
+  
     if (humanChoice == "Rock") {
-      if (computerChoice == "Rock") {
-        console.log("Nobody wins. Let's play again!");
+      if (computerSelection == "Rock") {
+        results.textContent = "Nobody wins. Let's play again!";
       }
-      else if (computerChoice == "Paper") {
-        console.log("Computer wins. Paper wraps Rock");
+      else if (computerSelection == "Paper") {
+        results.textContent = "Computer wins. Paper wraps Rock";
         computerScore++
       }
-      else if (computerChoice == "Scissors") {
-        console.log("You win!! Rock breaks scissors.");
+      else if (computerSelection == "Scissors") {
+        results.textContent = "You win!! Rock breaks scissors.";
         humanScore++
       }
     }
     else if (humanChoice == "Paper") {
-      if (computerChoice == "Rock") {
-        console.log("You win!! Paper wraps Rock")
+      if (computerSelection == "Rock") {
+        results.textContent = "You win!! Paper wraps Rock";
         humanScore++
       }
-      else if (computerChoice == "Paper") {
-        console.log("Nobody wins. Let's play again!");
+      else if (computerSelection == "Paper") {
+        results.textContent = "Nobody wins. Let's play again!";
       }
-      else if (computerChoice == "Scissors") {
-        console.log("Computer wins. Scissors cut paper.");
+      else if (computerSelection == "Scissors") {
+        results.textContent = "Computer wins. Scissors cut paper.";
         computerScore++
       }
     }
     else if (humanChoice == "Scissors") {
-      if (computerChoice == "Rock") {
-        console.log("Computer wins. Rock breaks scissors");
+      if (computerSelection == "Rock") {
+        results.textContent = "Computer wins. Rock breaks scissors";
         computerScore++
       }
-      else if (computerChoice == "Paper") {
-        console.log("You win!! Scissors cut paper");
+      else if (computerSelection == "Paper") {
+        results.textContent = "You win!! Scissors cut paper";
         humanScore++
       }
-      else if (computerChoice == "Scissors") {
-        console.log("Nobody wins. Let's play again!");
-
+      else if (computerSelection == "Scissors") {
+        results.textContent = "Nobody wins. Let's play again!";
+  
       }
     }
   }
 
+  function getComputerChoice() {
+    let choice = ""
+    let rand = Math.floor(Math.random() * 3)
+  
+    switch (rand) {
+      case 0:
+        choice = "Rock"
+        break
+      case 1:
+        choice = "Paper"
+        break
+      case 2:
+        choice = "Scissors"
+    }
+  
+    return choice
+  }
+  
   function winner(you, computer) {
     if (you > computer) {
       console.log("The winner is you!!!");
@@ -94,33 +104,3 @@ function playGame() {
       console.log('It\'s a tie.');
     }
   }
-
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-  playRound(humanSelection, computerSelection);
-  let score = `The score is, You: ${humanScore} to Comp: ${computerScore}`
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  playRound(humanSelection, computerSelection);
-  score = `The score is, You: ${humanScore} to Comp: ${computerScore}`
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  playRound(humanSelection, computerSelection);
-  score = `The score is, You: ${humanScore} to Comp: ${computerScore}`
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  playRound(humanSelection, computerSelection);
-  score = `The score is, You: ${humanScore} to Comp: ${computerScore}`
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  playRound(humanSelection, computerSelection);
-  score = `The score is, You: ${humanScore} to Comp: ${computerScore}`
-
-  return winner(humanScore, computerScore)
-}
-
-playGame()
